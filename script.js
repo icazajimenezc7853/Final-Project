4bfdabd033bdfc757ef6d81a5426d3a81557d6fc
icazajimenezc7853 <icazajimenezc7853@connect.durhamtech.edu>
2023-12-07 23:06:24 -0500

    Javascrip to individualize the code to be able to reuse it

--- script.js
+++ script.js
@@ -3,3 +3,28 @@
 function showAlert() {
     alert("Button clicked!");
 }
+
+function includeHTML() {
+    const headerContainer = document.getElementById("header");
+    const footerContainer = document.getElementById("footer");
+    const navigationContainer = document.getElementById("navigation");
+  
+    fetch("header.html")
+      .then(response => response.text())
+      .then(html => {
+        headerContainer.innerHTML = html;
+      });
+  
+    fetch("footer.html")
+      .then(response => response.text())
+      .then(html => {
+        footerContainer.innerHTML = html;
+      });
+  
+    fetch("navigation.html")
+      .then(response => response.text())
+      .then(html => {
+        navigationContainer.innerHTML = html;
+      });
+  }
+  
